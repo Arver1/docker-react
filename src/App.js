7cf32047de89c9b3053f71b8922c1ab409d9c250
Aleksandr Stepanov <astepanov9@alfabank.ru>
2021-06-11 16:41:39 +0300

    fix(*): test commit 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React more
+          Learn React more!
         </a>
       </header>
     </div>
